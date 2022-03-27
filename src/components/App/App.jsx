d222a4fe11041e0a936d4e82a50cf9a7a238e20e
Michael Chuinard <mpchuinard@finlandia.edu>
2022-03-26 20:58:52 -0500

    add css styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -43,7 +43,7 @@ const updateImage = (imageToUpdate) => {
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
+        <p>Photo Gallery</p>
         <main>
                 
                 {/* <div>
